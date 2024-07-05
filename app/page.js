167e2b3d214a5a76e228424d51e858684f6490c8
awwal148 <adesaanyaawwal446@gmail.com>
2024-07-05 14:49:45 -0700

    "Updated page.js and profile/page.js with new imports, functions, and UI components"

--- app/page.js
+++ app/page.js
@@ -3,6 +3,13 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const page = () => {
+  // useEffect(() => {
+  //   // Redirect to sign-up page if user is not authenticated
+  //   if (!user) {
+  //     router.push('/sign-in');
+  //     // console.log(user)
+  //   }
+  // }, [user, router]);
   return (
     <div>Hello world
        
