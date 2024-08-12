ffb4414df023f07f4b225ac6da1c4b820a0c999d
awwal148 <adesaanyaawwal446@gmail.com>
2024-08-12 07:43:57 -0700

    map functionality

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -7,6 +7,8 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+ 
+
   apiKey: "AIzaSyBMKVIkmpWP3uFrcXk8iW97wIKTdTvFiEM",
   authDomain: "real-estate-71347.firebaseapp.com",
   projectId: "real-estate-71347",
@@ -19,3 +21,45 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // import { getAnalytics } from "firebase/analytics";
+// import {getFirestore} from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+  // apiKey: "AIzaSyBr3oY8Jfn6Jgf_jIhl-HWbIYgi_Pa2GMU",
+  // authDomain: "real-estate-432122.firebaseapp.com",
+  // projectId: "real-estate-432122",
+  // storageBucket: "real-estate-432122.appspot.com",
+  // messagingSenderId: "359287119099",
+  // appId: "1:359287119099:web:8aa65991e73ea0e762e45e",
+  // measurementId: "G-M7GNWX5R1R"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app)
